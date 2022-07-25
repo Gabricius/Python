15161bc8f9dc46ac7000c479304d475ebe65ebd6
David <76440142+Gabricius@users.noreply.github.com>
2022-07-24 22:49:43 -0300

    Update pyLists.py

--- pyLists.py
+++ pyLists.py
@@ -1,35 +1,35 @@
 
 
-lists = [1, [2], [[3]], [[[4]]], [[[[[5]]]]]]
+list1 = [1, [2], [[3]], [[[4]]], [[[[[5]]]]]]
 # Try:
 # lists = [0, [1, [2, [3, [4, [5]]]]]]
 # lists = [0, 1, [3, [[4]]], [[[[5], [6]]]], [[[7, 8]], [9]], [[[[[10]]]]]]
 # lists = ["banana",["apple"],[[1,3]],[["juice"],[12]],[[[["PC"],[8]]],["hi"]],[[[[1]]]]]
 
-for v in lists:
-    if isinstance(v, list):
-        for x in range(0, len(v)):
+for item in list1:
+    if isinstance(item, list):
+        for x in range(0, len(item)):
 
-            if isinstance(v[x], list):
-                for y in range(0, len(v[x])):
+            if isinstance(item[x], list):
+                for y in range(0, len(item[x])):
 
-                    if isinstance(v[x][y], list):
-                        for z in range(0, len(v[x][y])):
+                    if isinstance(item[x][y], list):
+                        for z in range(0, len(item[x][y])):
 
-                            if isinstance(v[x][y][z], list):
-                                for a in range(0, len(v[x][y][z])):
+                            if isinstance(item[x][y][z], list):
+                                for a in range(0, len(item[x][y][z])):
 
-                                    if isinstance(v[x][y][z][a], list):
-                                        for b in range(0, len(v[x][y][z][a])):
+                                    if isinstance(item[x][y][z][a], list):
+                                        for b in range(0, len(item[x][y][z][a])):
 
-                                            print(v[x][y][z][a][b])
+                                            print(item[x][y][z][a][b])
                                     else:
-                                        print(v[x][y][z][a])
+                                        print(item[x][y][z][a])
                             else:
-                                print(v[x][y][z])
+                                print(item[x][y][z])
                     else:
-                        print(v[x][y])
+                        print(item[x][y])
             else:
-                print(v[x])
+                print(item[x])
     else:
-        print(v)
+        print(item)
