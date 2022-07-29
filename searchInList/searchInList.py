0527c826c16bf9e7a5b66c7b11c950332c746550
David <76440142+Gabricius@users.noreply.github.com>
2022-07-28 22:00:01 -0300

    Update searchInList.py

--- searchInList/searchInList.py
+++ searchInList/searchInList.py
@@ -1,16 +1,15 @@
-
-st = ''
-ctm = 0
-pNome = []
-ttl = []
-dep = []
-sal = []
-uNome = []
+nomeCompleto = ''
+listTitulos = []
+listDepartamentos = []
+listSalarios = []
+listPrimeiroNome = []
+listUltimoNome = []
+listNomeCompleto = []
 full_data = []
 
 # Busca e definição do arquivo
-f = open("C:/Users/Public/PythonF/arquivos/salarios.csv", 'r')
-data = f.read()
+with open("searchInList/salarios.csv", 'r') as f:
+    data = f.read()
 rows = data.split('\n')
 
 # Split (list in list) - Full data
@@ -18,56 +17,49 @@ for row in rows:
     split_row = row.split(",")
     full_data.append(split_row)
 
-
 for item in full_data:
+    coluna = 0
     # Consultar list in list
     for x in range(0, len(item)):
 
-        # Separa em listas individuais
-        if ctm != 4:
-            if ctm == 0:
-                pNome.append(item[x])
+        # Separa os items por coluna
+        if coluna == 0:
+            listPrimeiroNome.append(item[x])
 
-            if ctm == 1:
-                ttl.append(item[x])
+        if coluna == 1:
+            listTitulos.append(item[x])
 
-            if ctm == 2:
-                dep.append(item[x])
+        if coluna == 2:
+            listDepartamentos.append(item[x])
 
-            if ctm == 3:
-                sal.append(item[x])
+        if coluna == 3:
+            listSalarios.append(item[x])
 
-            ctm += 1
-            st += item[x] + ","
-        else:
-            uNome.append(item[x])
-            st += item[x]
-            ctm = 0
+        if coluna == 4:
+            listUltimoNome.append(item[x])
 
-nomeC = ''
-idx = 0
-listNomes = []
+        coluna += 1
 
 # Formação nome completo
-for idx in range(0, len(uNome)):
-    nomeC = pNome[(idx + 1)][2:-1] + " " + uNome[idx][1:]
-    if nomeC != '':
-        listNomes.append(nomeC)
+for idx in range(0, len(listUltimoNome)):
+    nomeCompleto = listPrimeiroNome[(idx + 1)][2:-1] + " " + listUltimoNome[idx][1:]
+    if nomeCompleto != '':
+        listNomeCompleto.append(nomeCompleto)
 
 # Lista tudo
-for z in range(0, len(listNomes)):
-    if listNomes[z] != ' ':
-        print("\nNome: " + listNomes[z])
-        print("Salário: " + sal[z + 1])
-        print("Título: " + ttl[z + 1])
-        print("Departamento: " + dep[z + 1] + "\n")
+for counter in range(0, len(listNomeCompleto)):
+    if listNomeCompleto[counter] != ' ':
+        print("\nNome: " + listNomeCompleto[counter])
+        print("Salário: " + listSalarios[counter + 1])
+        print("Título: " + listTitulos[counter + 1])
+        print("Departamento: " + listDepartamentos[counter + 1] + "\n")
 
 # Faz a busca da linha especificada
 """
-i = int(input("Qual o número do registro? "))  # -2  # Igual na lista
+indexItem = int(input("Qual o número do registro? "))  # -2  # Igual na lista
 
-print("\nNome: " + listNomes[i])
-print("Salário: " + sal[i + 1])
-print("Título: " + ttl[i + 1])
-print("Departamento: " + dep[i + 1] + "\n")
+print("\nNome: " + listNomeCompleto[indexItem])
+print("Salário: " + listSalarios[indexItem + 1])
+print("Título: " + listTitulos[indexItem + 1])
+print("Departamento: " + listDepartamentos[indexItem + 1] + "\n")
 """
